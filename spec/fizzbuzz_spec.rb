b43f07152c0219fbdf39bb5fadca8aa7bc224607
Mike Jeuga <mikejeuga@gmail.com>
2021-03-19 18:17:18 +0000

    fix for every other case.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,8 @@ describe "fizzbuzz" do
   it 'returns "fizzbuzz"' do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
+
+  it "returns number otherwise" do
+    expect(fizzbuzz(7)).to eq 7
+  end
 end
