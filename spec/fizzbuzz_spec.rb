79ceff9eefcb44fec28bc1f0682ef864edc62808
Mike Jeuga <mikejeuga@gmail.com>
2021-03-20 17:31:30 +0000

    Additional expamles to the test.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,7 +13,23 @@ describe "fizzbuzz" do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
 
-  it "returns number otherwise" do
+  it "returns the number in any other case" do
     expect(fizzbuzz(7)).to eq 7
+  end
+
+  it "returns the number in any other case" do
+    expect(fizzbuzz(4)).to eq 4
+  end
+
+  it 'returns "fizz"' do
+    expect(fizzbuzz(27)).to eq "fizz"
+  end
+
+  it 'returns "buzz"' do
+    expect(fizzbuzz(50)).to eq "buzz"
+  end
+
+  it 'returns "fizzbuzz"' do
+    expect(fizzbuzz(45)).to eq "fizzbuzz"
   end
 end
